42477c3d9993ac6065f1d5228af3ee262185b95f
Default user <js.espitia@uniandes.edu.co>
2019-02-06 01:00:19 +0000

    Previous part done

--- hw1/problem2.sh
+++ hw1/problem2.sh
@@ -1,3 +1,3 @@
-wget https://github.com/jmsevillam/Herramientas-Computacionales-UniAndes/blob/master/Data/DataHw1a.dat
-
-
+wget https://raw.githubusercontent.com/jmsevillam/Herramientas-Computacionales-UniAndes/master/Data/DataHw1a.dat
+grep -w j.sevillam DataHw1a.dat > data2.dat
+ 
---
+++ hw1/problem4.sh
@@ -0,0 +1,7 @@
+wget https://github.com/jmsevillam/Herramientas-Computacionales-UniAndes/blob/master/Data/DataHw1b.zip?raw=true
+unzip DataHw1b.zip?raw=true
+cd DataHw1b
+cat file1.txt file2.txt file3.txt
+#When executing cat with 3 file arguments, it prints the content of the three files sequentially in the same order they were indicated
+
+
